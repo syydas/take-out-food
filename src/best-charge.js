f603364c337efa5e7ecd48c54bb40dee9a924715
Asuu <suqinzheng_0624@163.com>
2020-01-08 14:55:49 +0800

    [zhengsuqin]feat: organize the code structure with the Tasking of take-out-food app

--- src/best-charge.js
+++ src/best-charge.js
@@ -1,3 +1,21 @@
 function bestCharge(selectedItems) {
-  return /*TODO*/;
+  let ItemsList = getItemsList(selectedItems);
+  let summary = hasDiscount(ItemsList);
+  let result = printTickets(ItemsList, summary);
+  return result;
+}
+
+function getItemsList(selectedItems) {
+  const allItems = loadAllItems();
+  return ItemsList;
+}
+
+function hasDiscount(ItemsList) {
+  let summary = {};
+  return summary;
+}
+
+function printTickets(ItemsList, summary) {
+  let result = '============= 订餐明细 =============';
+  return result;
 }
