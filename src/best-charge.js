b125178649bd4cce5a76291ff5a910ff5dd25ec1
Asuu <suqinzheng_0624@163.com>
2020-01-08 18:36:56 +0800

    [zhengsuqin]feat: finish the print tickets function of the take-out-food app

--- src/best-charge.js
+++ src/best-charge.js
@@ -1,10 +1,9 @@
 function bestCharge(selectedItems) {
   let itemsList = getItemsList(selectedItems);
   let summary = hasDiscount(itemsList);
-  console.log(summary);
-  //let result = printTickets(ItemsList, summary);
-  //return result;
-  return /*TODO*/;
+  let result = printTickets(itemsList, summary);
+  console.log(result);
+  return result;
 }
 
 function getItemsList(selectedItems) {
@@ -20,47 +19,6 @@ function getItemsList(selectedItems) {
   return ItemsList;
 }
 
-function loadAllItems() {
-  return [
-    {
-      id: "ITEM0001",
-      name: "黄焖鸡",
-      price: 18.0
-    },
-    {
-      id: "ITEM0013",
-      name: "肉夹馍",
-      price: 6.0
-    },
-    {
-      id: "ITEM0022",
-      name: "凉皮",
-      price: 8.0
-    },
-    {
-      id: "ITEM0030",
-      name: "冰锋",
-      price: 2.0
-    }
-  ];
-}
-
-function loadPromotions() {
-  return [
-    {
-      type: "满30减6元"
-    },
-    {
-      type: "指定菜品半价",
-      items: ["ITEM0001", "ITEM0022"]
-    }
-  ];
-}
-
-
-let inputs = ["ITEM0013 x 4", "ITEM0022 x 1"];
-bestCharge(inputs);
-
 function hasDiscount(itemsList) {
   let summary = {};
   let totalPrice = itemsList.reduce((price, item) => {
@@ -71,25 +29,52 @@ function hasDiscount(itemsList) {
   let promotions = loadPromotions();
   promotions[1].items.forEach(value => {
     itemsList.forEach(item => {
-      if(item.id === value) {
+      if (item.id === value) {
         halfPrice += item.price / 2;
         discountHalfItem.push(item.name);
       }
     });
   });
   if (totalPrice < 30) {
-    summary.discount = halfPrice ? halfPrice : "";
-    summary.totalPrice = totalPrice - summary.discount;
-    summary.type = halfPrice ? promotions[1].type : "";
+    if (halfPrice) {
+      summary.discount = halfPrice;
+      summary.totalPrice = totalPrice - halfPrice;
+      summary.type = promotions[1].type;
+      summary.discountItem = discountHalfItem;
+    } else {
+      summary.totalPrice = totalPrice;
+    }
   } else {
     summary.discount = Math.max(halfPrice, 6);
     summary.totalPrice = totalPrice - summary.discount;
-    summary.type = 6 > halfPrice ? promotions[0].type :promotions[1].type;
+    summary.type = 6 > halfPrice ? promotions[0].type : promotions[1].type;
+    summary.discountItem = 6 > halfPrice ? "" : discountHalfItem;
   }
   return summary;
 }
 
-function printTickets(ItemsList, summary) {
-  let result = "============= 订餐明细 =============";
+function printTickets(itemsList, summary) {
+  let result = "============= 订餐明细 =============\n";
+  itemsList.forEach(item => {
+    return (result += `${item.name} x ${item.count} = ${item.subTotalPrice}元\n`);
+  });
+  result += "-----------------------------------\n";
+  switch (summary.type) {
+    case "满30减6元":
+      result += `使用优惠:\n${summary.type}，省${summary.discount}元\n`;
+      result += "-----------------------------------\n";
+      break;
+    case "指定菜品半价":
+      result += `使用优惠:\n${summary.type}(${summary.discountItem.join(
+        "，"
+      )})，省${summary.discount}元\n`;
+      result += "-----------------------------------\n";
+      break;
+    default:
+      break;
+  }
+
+  result += `总计：${summary.totalPrice}元\n`;
+  result += "===================================\n";
   return result;
 }
